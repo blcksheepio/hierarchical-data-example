b7ec338f27056f9283f2b9898bbe0f5a31dd8315
Jason Lamb <jason@ridango.com>
2018-06-26 08:11:35 +0300

    5b3087c8cf37d191eb7e79b1 Symfony - Neo4J
    Initial installation
    
    - Needed to add BCMatch PHP extension
    - Added neo4j/neo4j-bundle + graphaware/neo4j-php-ogm:@rc
    - Provided initial Yaml configuration

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Neo4j\Neo4jBundle\Neo4jBundle::class => ['all' => true],
 ];

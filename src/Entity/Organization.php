36471faacf93102125680bd0dc375d581263c421
Jason Lamb <jason@ridango.com>
2018-06-26 20:52:11 +0300

    5b31d4526fd4cfeed9d33dfb Symfony - Organization entity
    Started defining the relationships on the Organization entity.

--- src/Entity/Organization.php
+++ src/Entity/Organization.php
@@ -28,16 +28,121 @@ class Organization
 
     /**
      * @var Organization[] | Collection
+     * @OGM\Relationship(
+     *     type="PARENT",
+     *     direction="OUTGOING",
+     *     collection=true,
+     *     mappedBy="daughters",
+     *     targetEntity="Organization"
+     * )
      */
     protected $parents;
 
     /**
      * @var Organization[] | Collection
+     * @OGM\Relationship(
+     *     type="DAUGHTER",
+     *     direction="INCOMING",
+     *     collection=true,
+     *     mappedBy="parents",
+     *     targetEntity="Organization"
+     * )
      */
-    protected $children;
+    protected $daughters;
 
     /**
      * @var Organization[] | Collection
+     * @OGM\Relationship(
+     *     type="SISTER",
+     *     direction="BOTH",
+     *     collection=true,
+     *     mappedBy="sisters",
+     *     targetEntity="Organization"
+     * )
      */
     protected $sisters;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Organization
+     */
+    public function setName(string $name): Organization
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Organization[]|Collection
+     */
+    public function getParents()
+    {
+        return $this->parents;
+    }
+
+    /**
+     * @param Organization[]|Collection $parents
+     * @return Organization
+     */
+    public function setParents($parents)
+    {
+        $this->parents = $parents;
+
+        return $this;
+    }
+
+    /**
+     * @return Organization[]|Collection
+     */
+    public function getDaughters()
+    {
+        return $this->daughters;
+    }
+
+    /**
+     * @param Organization[]|Collection $daughters
+     * @return Organization
+     */
+    public function setDaughters($daughters)
+    {
+        $this->daughters = $daughters;
+
+        return $this;
+    }
+
+    /**
+     * @return Organization[]|Collection
+     */
+    public function getSisters()
+    {
+        return $this->sisters;
+    }
+
+    /**
+     * @param Organization[]|Collection $sisters
+     * @return Organization
+     */
+    public function setSisters($sisters)
+    {
+        $this->sisters = $sisters;
+
+        return $this;
+    }
 }

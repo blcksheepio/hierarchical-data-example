5f954c2a6ae091bb844c77ebd371da874b4339bc
Jason Lamb <jason@ridango.com>
2018-07-01 10:32:08 +0300

    Added initial OrganizationRepository

---
+++ src/Repository/OrganizationRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repository;
+
+use GraphAware\Neo4j\OGM\Repository\BaseRepository;
+
+/**
+ * Class OrganizationRepository
+ *
+ * @author blcksheep
+ */
+class OrganizationRepository extends BaseRepository
+{
+
+}
